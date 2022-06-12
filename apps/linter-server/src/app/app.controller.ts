21ff5e8ed505fda743cbf1593d1311fa118431fb
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2022-06-12 09:00:51 -0700

    Initial COmmit

--- apps/linter-server/src/app/app.controller.ts
+++ apps/linter-server/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -8,6 +8,11 @@ export class AppController {
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return this.appService.getHelp();
+  }
+
+  @Post('linter')
+  postLinter() {
+    return this.appService.postLinter();
   }
 }
--- apps/linter-server/src/app/app.service.ts
+++ apps/linter-server/src/app/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+export interface HelpMessage {
+  api: {
+    title: string;
+  };
+  resource: string;
+}
+
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     return { message: 'Welcome to linter-server!' };
+  }
+
+  getHelp(): { message: HelpMessage } {
+    return {
+      message: {
+        api: {
+          title: 'API Spec Linter'
+        },
+        resource: 'v1/linter',
+      }
+    };
+  }
+
+  postLinter(): { message: string } {
+    return { message: 'Linter is running' };
   }
 }
